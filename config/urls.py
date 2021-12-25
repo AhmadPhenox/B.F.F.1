de19d81e6d4fa06f2f1dbfde39d978491a683616
AhmadPhenox <ahmaddhyaa4@gmail.com>
2021-12-25 15:27:15 -0800

    second commit

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
+
+
 
 from account.views import account_controller
 from commerce.views import commerce_controller
@@ -30,3 +34,5 @@ urlpatterns = [
     path('api/', api.urls),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
